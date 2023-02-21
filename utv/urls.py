2c633b4ff4132297247847e8bfad457690748266
chudilo4 <chipalinchik@gmail.com>
2023-02-21 18:00:43 +0500

    Добавил Cache

--- utv/urls.py
+++ utv/urls.py
@@ -27,10 +27,9 @@ urlpatterns = [
     # path('', include('utv_smeta.urls')),
     path('api/v1/', include('utv_api.urls')),
 ]
-
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns.append(('__debug__/', include('debug_toolbar.urls')))
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += doc_url
