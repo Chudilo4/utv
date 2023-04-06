3e2af9276b6043de1690ed0b73c6f3b64cf6e790
chudilo4 <chipalinchik@gmail.com>
2023-04-06 14:33:18 +0500

    поправил код

--- sec.py
+++ sec.py
@@ -1,9 +1,6 @@
 from django.core.management.utils import get_random_secret_key
 import socket
 hostname = socket.gethostname()
-IPAddr = socket.gethostbyname(hostname)
 with open('.env', 'w') as file:
-    file.write(f'SECRET_KEY="{get_random_secret_key()}"\n')
-    file.write(f'DEBUG=1\n')
-    file.write(f'ALLOWED_HOSTS="{hostname} 127.0.0.1"')
-
+    file.write(f'SECRET_KEY="{get_random_secret_key()}"\n'
+               f'DEBUG=1\nALLOWED_HOSTS="{hostname} 127.0.0.1"')
